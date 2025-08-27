e1df8d67e5c5760a4386d7e5c419e7f160aa20e2
Archit Kumar <architk471@gmail.com>
2025-08-27 22:01:30 +0530

    fixed some issues

--- myproject/settings.py
+++ myproject/settings.py
@@ -161,8 +161,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=20),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
